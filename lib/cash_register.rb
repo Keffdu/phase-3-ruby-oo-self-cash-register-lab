ccb182b1eb6a1b75d87e8e13bc953196a9f4e699
Kevin Dutzy <kddutz15@gmail.com>
2022-12-14 22:40:47 -0500

    commit tree dfeb20362fbe8586d1722e96042774afc587ad1f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+require 'pry'
+class CashRegister
+attr_accessor :total, :discount, :items, :last_transaction
+    
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
 
+    def add_item(title, price, quantity = 1)
+        self.last_transaction = price * quantity
+        self.total += self.last_transaction
+        quantity.times do 
+            self.items << title
+        end
+
+        def apply_discount
+            self.total = self.total / self.discount
+            # binding.pry
+        end
+    end
+
+
+end
+
+cart = CashRegister.new(20)
+cart.add_item("macbook", 500)
+cart.apply_discount
